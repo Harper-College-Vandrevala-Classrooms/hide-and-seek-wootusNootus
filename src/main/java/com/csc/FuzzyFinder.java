b3690c1374f082a166b60ebce46223d95137ea90
wootusNootus <migospodinoff@gmail.com>
2024-10-27 20:43:20 -0500

    Tests work

--- src/main/java/com/csc/FuzzyFinder.java
+++ src/main/java/com/csc/FuzzyFinder.java
@@ -1,7 +1,42 @@
 package com.csc;
 
+import java.util.ArrayList;
+
 public class FuzzyFinder {
-  // Your code goes here!
+  
+  public int linearSearch(ArrayList<Fuzzy> array){
+    for(int i = 0; i < array.size(); i++){
+      if(array.get(i).color.equals("gold")){
+        return i;
+      }
+    }
+
+    return -1; // return -1 if not found
+  }
+
+  public int binarySearch(ArrayList<Fuzzy> array){
+    int l = 0;
+    int r = array.size() - 1;
+
+    while (l <= r){
+      int m = l + (r - l) / 2; // "splitting" array
+      int compare = array.get(m).color.compareTo("gold"); // checking to see if middle is gold fuzzy
+
+      if(compare == 0){
+        return m; 
+      }
+      else if (compare < 0){
+        l = m + 1; // lessen bounds to 2nd half of split
+      }
+      else{
+        r = m - 1; // lessen bounds to 1st half of split
+      }
+    }
+
+    return -1;
+  }
+
+
   public static void main(String args[]) {
     System.out.println("In addition to your search functions, you can write a main function if you wish!");
   }
--- src/test/java/com/csc/TestFuzzyFinder.java
+++ src/test/java/com/csc/TestFuzzyFinder.java
@@ -29,4 +29,33 @@ public class TestFuzzyFinder {
     ArrayList<Fuzzy> fuzzies = generator.sortedRainbowFuzzies();
     assertEquals("purple", fuzzies.getFirst().color);
   }
+
+  @Test
+  public void testLinearSearchOnSortedList() {
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+    int index = finder.linearSearch(sortedFuzzies);
+    assertEquals("gold", sortedFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testLinearSearchOnRandomList() {
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+    int index = finder.linearSearch(randomFuzzies);
+    assertEquals("gold", randomFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testBinarySearchOnSortedList() {
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+    int index = finder.binarySearch(sortedFuzzies);
+    assertEquals("gold", sortedFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testBinarySearchOnRandomizedList() {
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+    int index = finder.binarySearch(randomFuzzies);
+    assertEquals("gold", randomFuzzies.get(index).color);
+  }
+
 }
